5c798f80d9dcd4c9946ba13d435e37c942d57a67
Bharadwaj-vinay <102237792+Bharadwaj-vinay@users.noreply.github.com>
2025-04-06 17:27:18 +0530

    exploring HTTP Methods

--- src/app.js
+++ src/app.js
@@ -3,16 +3,26 @@ const express = require('express');
 const app = express();
 //creates an instance of express, our router
 
-app.use("/", (req, res) => {
-    res.send('Namaste from the Author!');
+// this method will match only the HTTP GET API calls to /user
+app.get("/user", (req, res) => {
+    res.send({
+        name: "Bhradwaj",
+        age: 28,
+    });
 });
 
+app.post("/user", (req, res) => {
+    console.log("User Created");
+    res.send("Data saved successfully");
+});
+
+app.delete("/user", (req, res) => {
+    res.send("Data deleted successfully");
+});
+
+// this method will match all the HTTP method API calls to /test
 app.use("/test", (req, res) => {
-    res.send('Namaste from the Test!');
-});
-
-app.use("/hello", (req, res) => {
-    res.send('Hello Hello Hello!!!');
+    res.send('Hello FROM THE Server!');
 });
 
 app.listen(7777, () => {
