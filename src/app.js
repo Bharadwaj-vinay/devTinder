32d9a396d4f0d898d0c208b8470a0ff5b1410512
Bharadwaj-vinay <102237792+Bharadwaj-vinay@users.noreply.github.com>
2025-04-20 16:20:21 +0530

    user model APIs

--- src/app.js
+++ src/app.js
@@ -4,15 +4,14 @@ const connectToDB = require('./config/database');
 const app = express();
 const User = require("./models/user");
 
+app.use(express.json());
+// Middleware to parse JSON data from the request body
+
 app.post("/signup", async (req, res) => {
 
   // creating a new instance of the User model with the current user data
-  const user = new User({
-    firstName: "Ajay",
-    lastName: "Kumar",
-    emailId: "ajay@kumar.com",
-    password: "ajay@123",
-  });
+  const user = new User(req.body);
+
   try {
     // saving the user instance to the database 
     await user.save();
@@ -25,6 +24,61 @@ app.post("/signup", async (req, res) => {
     // 403 - Forbidden
     // 404 - Not Found
     res.status(400).send("Error adding user: " + error.message);
+  }
+});
+
+app.get('/user', async (req, res) => {
+  try {
+    const user = await User.findOne({emailId: req.body.emailId});
+    res.send(user);
+    //const users = await User.find({emailId: req.body.emailId});
+
+    // if  (!users.length) {
+    //   return res.status(404).send("User not found");
+    // } else {
+    //   // If user is found, send the user data as a response
+    //   res.send(users);
+    // }
+  } catch (error) {
+    res.status(400).send("Error fetching user: " + error.message);
+  }
+});
+
+app.get('/feed', async (req, res) => {
+  try {
+    const users = await User.find({});
+    // {} as no filter to fetch all users
+      res.send(users);
+  } catch (error) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+app.delete('/user', async (req,res) => {
+  const userId = req.body.userId; 
+  try {
+    const user = await User.findByIdAndDelete(userId); 
+    // If user is not found, send a 404 response
+    if (!user) {
+      return res.status(404).send("User not found");
+    } else {
+      // If user is found, send the user data as a response
+      res.send("User deleted successfully");
+    }
+  } catch  (error) {
+    res.status(400).send("Error deleting user");
+  }
+});
+
+app.patch('/user', async (req, res) => {
+  const data = req.body;
+  const userId = req.body.userId;
+
+  try {
+    await User.findByIdAndUpdate(userId, data);
+    res.send("User updated successfully");
+  } catch (error) {
+    res.status(400).send("Something went wrong");
   }
 });
 
