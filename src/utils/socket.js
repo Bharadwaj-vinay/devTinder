9e9f3a9295fdeaaf782a924815eb7243cc639ad1
Bharadwaj-vinay <102237792+Bharadwaj-vinay@users.noreply.github.com>
2025-05-09 11:47:38 +0530

    socket events & handling

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,6 +1,14 @@
 const socket = require("socket.io");
+const crypto = require("crypto");
 
-const intializeSocket = server => {
+const getSecretRoomId = (userId, targetUserId) => {
+    return crypto
+        .createHash("sha256")
+        .update([userId, targetUserId].sort().join("_"))
+        .digest("hex");
+};
+
+const intializeSocket = server => { 
     const io = socket(server, {
         cors: {
             origin: "http://localhost:5173",
@@ -9,12 +17,19 @@ const intializeSocket = server => {
 
     io.on("connection", socket => {
         //handle socket events 
-        socket.on("joinChat", () => {
+        socket.on("joinChat", ({userId, targetUserId}) => {
+             const roomId = [userId, targetUserId].sort().join("_");
+             console.log('joinchat roomid', roomId);
 
+             //creating a unique chat room for these 2 users
+             socket.join(roomId);
         });
 
-        socket.on("sendMessage", () => {
-            
+        socket.on("sendMessage", 
+            ({firstName, userId, targetUserId, text}) => {
+                const roomId = getSecretRoomId(userId, targetUserId);
+                console.log('sendmsg roomid', roomId);
+                io.to(roomId).emit("messageReceived", {text, firstName});
         });
 
         socket.on("disconnect", () => {
